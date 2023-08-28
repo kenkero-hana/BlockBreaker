3ade467038d3af1bf63309510cefab0def4594bb
kenkero-hana <watanabe.kenkanfu@gmail.com>
2023-08-28 16:39:06 +0900

    0828_完成

--- actenemy.c
+++ actenemy.c
@@ -2,24 +2,24 @@
 
 #include "function.h"
 // 敵
-void actenemy(struct param* actor, struct param* target, int enemycommand){
+void actenemy(struct param* target, struct param* actor, int enemycommand){
     int enepoint;
     switch (enemycommand)
     {
     case 3:// 体力
-        enepoint = actor->attack - target->defense;
-        target->life -= enepoint;
-        printf("%s attacks! %s takes %d damage points!\n", actor->name, target->name, enepoint);
+        enepoint = target->attack - actor->defense;
+        actor->life -= enepoint;
+        printf("%s attacks! %s takes %d damage points!\n", target->name, actor->name, enepoint);
         break;
     case 4://気合いだめ
         enepoint = 20;
-        actor->attack += enepoint;
-        printf("%s casts Boost! %s's Attack increases by %d!\n", actor->name, actor->name, enepoint);
+        target->attack += enepoint;
+        printf("%s casts Boost! %s's Attack increases by %d!\n", target->name, actor->name, enepoint);
         break;
     case 5:// ガード
         enepoint = 50;
-        actor->life += enepoint;
-        printf("%s casta Heal! %s's Life increases by %d!\n", actor->name, actor->name, enepoint);
+        target->life += enepoint;
+        printf("%s casta Heal! %s's Life increases by %d!\n", target->name, target->name, enepoint);
         break;
     default:
         break;
